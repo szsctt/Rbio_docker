e070a06195839ffaf6b1ad76f6a202800fab76b7
szsctt <suzannesctt@gmail.com>
2022-03-21 15:03:31 +1000

    Added markdown and rmarkdown

--- install.R
+++ install.R
@@ -1,7 +1,7 @@
 install.packages(c("cowplot", "writexl", "wesanderson", "tidyverse", "ggrepel", "DT", "tidyverse", 
 					"BiocManager", "circlize", "furrr", "kableExtra", "magick", "multidplyr",
 					"patchwork", "modelr", "pheatmap", "plotly", "tidyquant", "ggwordcloud", 
-					"wordcloud", "wordcloud2"))
+					"wordcloud", "wordcloud2", "markdown", "rmarkdown"))
 
 BiocManager::install(version="3.14")
 BiocManager::install(c("geneRxCluster", "karyoploteR", "biomaRt", "GenomicRanges"))
