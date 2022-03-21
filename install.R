2d4ea07fb322f5052b7fcffaaa77e408a20a75ee
Suzanne Scott <suzanne.scott@csiro.au>
2022-03-21 15:13:11 +1100

    added more packages to install script

--- install.R
+++ install.R
@@ -1,5 +1,7 @@
 install.packages(c("cowplot", "writexl", "wesanderson", "tidyverse", "ggrepel", "DT", "tidyverse", 
-					"BiocManager"))
+					"BiocManager", "circlize", "furrr", "kableExtra", "magick", "multidplyr",
+					"patchwork", "modelr", "pheatmap", "plotly", "tidyquant", "ggwordcloud", 
+					"wordcloud", "wordcloud2"))
 
 BiocManager::install(version="3.14")
 BiocManager::install(c("geneRxCluster", "karyoploteR", "biomaRt", "GenomicRanges"))
