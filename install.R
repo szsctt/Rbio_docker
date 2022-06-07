645830c258fa0eb00e4d5eae611547f6d5b1b1cd
Suzanne Scott <suzannesctt@gmail.com>
2022-06-07 14:25:50 +1000

    added tantalus

--- install.R
+++ install.R
@@ -1,7 +1,10 @@
 install.packages(c("cowplot", "writexl", "wesanderson", "ggrepel", "DT", "tidyverse", 
 					"BiocManager", "circlize", "furrr", "kableExtra", "magick", "multidplyr",
 					"patchwork", "modelr", "pheatmap", "plotly", "tidyquant", "ggwordcloud", 
-					"wordcloud", "wordcloud2", "markdown", "rmarkdown", "ggplotify", "httr", "jsonlite"))
+					"wordcloud", "wordcloud2", "markdown", "rmarkdown", "ggplotify", "httr", "jsonlite", "devtools"))
 
 BiocManager::install(version="3.14")
 BiocManager::install(c("geneRxCluster", "karyoploteR", "biomaRt", "GenomicRanges", "edgeR", "DESeq2"))
+
+devtools::intstall_github("serratus-bio/tantalus")
+
