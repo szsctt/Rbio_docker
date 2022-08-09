cc7a03c9eaa7e5722c948d364647b011f777db0f
Suzanne Scott <suzannesctt@gmail.com>
2022-08-09 11:50:02 +1000

    added rmspc

--- install.R
+++ install.R
@@ -5,4 +5,5 @@ install.packages(c("cowplot", "writexl", "wesanderson", "ggrepel", "DT", "tidyve
 					"jsonlite", "reutils", "here"))
 
 BiocManager::install(version="3.14")
-BiocManager::install(c("geneRxCluster", "karyoploteR", "biomaRt", "GenomicRanges", "edgeR", "DESeq2", "TxDb.Hsapiens.UCSC.hg38.knownGene", "org.Hs.eg.db"))
+BiocManager::install(c("geneRxCluster", "karyoploteR", "biomaRt", "GenomicRanges", "edgeR", 
+						"DESeq2", "TxDb.Hsapiens.UCSC.hg38.knownGene", "org.Hs.eg.db", "rmspc"))
