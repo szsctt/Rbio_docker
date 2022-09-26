1d2ff91c38dfa356f57dcbf062af5e1e7106be68
Suzanne Scott <suzannesctt@gmail.com>
2022-09-26 10:35:58 +1000

    need bioconductor version 3.15 for R 4.2

--- install.R
+++ install.R
@@ -5,6 +5,6 @@ install.packages(c("cowplot", "writexl", "wesanderson", "ggrepel", "DT", "tidyve
 					"jsonlite", "reutils", "here", "ggforce", "rstatix", "argparse", "seqinr",
 					"umap"))
 
-BiocManager::install(version="3.14")
+BiocManager::install(version="3.15")
 BiocManager::install(c("geneRxCluster", "karyoploteR", "biomaRt", "GenomicRanges", "edgeR", 
 						"DESeq2", "TxDb.Hsapiens.UCSC.hg38.knownGene", "org.Hs.eg.db", "rmspc"))
