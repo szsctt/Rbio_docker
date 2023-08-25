52f60e8575d9580c04786864377a6b18f0618461
szsctt <suzannesctt@gmail.com>
2023-08-25 13:18:35 +1000

    Update install.R

--- install.R
+++ install.R
@@ -3,8 +3,9 @@ install.packages(c("cowplot", "writexl", "wesanderson", "ggrepel", "DT", "tidyve
 					"patchwork", "modelr", "pheatmap", "plotly", "tidyquant", "ggwordcloud", 
 					"wordcloud", "wordcloud2", "markdown", "rmarkdown", "ggplotify", "httr", 
 					"jsonlite", "reutils", "here", "ggforce", "rstatix", "argparse", "seqinr",
-					"umap", "tm", "SnowballC", "ggtext", "gtools", "Peptides", "ggbeeswarm"))
+					"umap", "tm", "SnowballC", "ggtext", "gtools", "Peptides", "ggbeeswarm",
+		  			"GOxploreR"))
 
 BiocManager::install(version="3.15", ask=FALSE)
-BiocManager::install(c("geneRxCluster", "karyoploteR", "biomaRt", "GenomicRanges", "edgeR", 
+BiocManager::install(c("geneRxCluster", "karyoploteR", "biomaRt", "GenomicRanges", "edgeR", "GO.db",
 						"DESeq2", "TxDb.Hsapiens.UCSC.hg38.knownGene", "org.Hs.eg.db", "rmspc"))
