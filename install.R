48e559864ecb61262738baadf545b699922b2395
Suzanne Scott <23192453+szsctt@users.noreply.github.com>
2024-04-09 18:00:03 +1000

    fixed missing quote in install.R

--- install.R
+++ install.R
@@ -7,7 +7,7 @@ install.packages(c("cowplot", "writexl", "wesanderson", "ggrepel", "DT", "tidyve
 		   "umap", "tm", "SnowballC", "ggtext", "gtools", "Peptides", "dbscan", 
                     "figpatch", "RMariaDB"))
 
-BiocManager::install(version="3.18, ask=FALSE)
+BiocManager::install(version="3.18", ask=FALSE)
 BiocManager::install(c("geneRxCluster", "karyoploteR", "biomaRt", "GenomicRanges", "edgeR", 
 			"DESeq2", "TxDb.Hsapiens.UCSC.hg38.knownGene", "org.Hs.eg.db", "topGO",
                         "Rgraphviz", "GO.db", "ggtree", "rhdf5", "AnnotationHub"))
